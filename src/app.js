58cc333750cb1079017571d84f16c15ec1590757
anasedf <anasandfred@gmail.com>
2025-04-13 22:11:20 +0700

    cors

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors'); // เพิ่มบรรทัดนี้
 
 const configRoutes = require('./routes/configs');
 const statusRoutes = require('./routes/status');
 const logsRoutes = require('./routes/logs');
 
+app.use(cors()); // เพิ่มบรรทัดนี้เพื่อใช้ cors middleware
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('🛫 Drone API is ready! Use /configs/:id, /logs/:id, /status/:id');
   });
-  
-app.use(cors());
-
-app.use(express.json());
 
 app.use('/configs', configRoutes);
 app.use('/status', statusRoutes);
